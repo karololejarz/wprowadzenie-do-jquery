3de255d91554bc985539a906785d35fc24cf607e
karololejarz <karol1olejarz@gmail.com>
2018-04-18 23:09:00 +0200

    10.3 ambitniej

--- 10-3/js/script.js
+++ 10-3/js/script.js
@@ -1,18 +1,137 @@
 $(function(){
 
-	var carouselList = $('#carousel ul');
+  var carouselList = $('#carousel ul');
+  var currentSlide = 1;
+  
+  setInterval(slideForward, 30000);
 
-  setInterval(changeSlide, 3000);
-
-  function moveSlide() {
+  function moveSlideForward() {
     var firstItem = carouselList.find('li:first');
     var lastItem = carouselList.find('li:last');
     lastItem.after(firstItem);
-    carouselList.css({marginLeft:0});
+    carouselList.css({marginLeft:0});     
   };
 
-  function changeSlide() {
-    carouselList.animate({'marginLeft':-400}, 500, moveSlide);
+  function moveSlideBackward() {
+    var firstItem = carouselList.find('li:first');
+    var lastItem = carouselList.find('li:last');
+    firstItem.before(lastItem);
+    carouselList.css({marginRight:400});    
   };
+
+  function slideForward() {
+    carouselList.animate({'marginLeft':-400}, 500, moveSlideForward);
+  };
+
+  function slideBackward() {
+    carouselList.animate({'marginRight':-400}, 500, moveSlideBackward);
+  };
+
+  function moveRadioBackward() {
+    currentSlide--;
+    if (currentSlide != 0) {
+      $("input[name=radio]:checked").prevAll(':radio:first').prop('checked', true);
+    } else {
+      $("input[name=radio]:last").prop('checked', true);
+      currentSlide = 5;
+    }
+  }
+
+  function moveRadioForward() {
+    currentSlide++;
+    if(currentSlide != 6) {
+      $("input[name=radio]:checked").nextAll(':radio:first').prop('checked', true);
+    } else {      
+      $("input[name=radio]:first").prop('checked', true);
+      currentSlide = 1;
+    }    
+  }
+
+  $('#left-scroll').click(function() {
+    slideBackward();
+    moveRadioBackward();
+  });
+
+  $('#right-scroll').click(function() {
+    slideForward();
+    moveRadioForward();
+  });
+
+  $('#radio1').click(function() {
+    if(currentSlide == 2) {slideBackward();}
+    if(currentSlide == 3) {
+      slideBackward();
+      slideBackward();
+    }
+    if(currentSlide == 4) {
+      slideForward();
+      slideForward();
+    }
+    if(currentSlide == 5) {slideForward();}
+
+    currentSlide = 1
+  });
+
+  $('#radio2').click(function() {
+    if(currentSlide == 1) {slideForward();}
+    if(currentSlide == 3) {slideBackward();}
+    if(currentSlide == 4) {
+      slideBackward();
+      slideBackward();
+    }
+    if(currentSlide == 5) {
+      slideForward()
+      slideForward()
+    ;}
+
+    currentSlide = 2
+  });
+
+  $('#radio3').click(function() {
+    if(currentSlide == 1) {
+      slideForward();
+      slideForward();
+    }
+    if(currentSlide == 2) {slideForward();}
+    if(currentSlide == 4) {
+      slideBackward();
+    }
+    if(currentSlide == 5) {
+      slideForward()
+      slideForward()
+    ;}
+
+    currentSlide = 3
+  });
+
+  $('#radio4').click(function() {
+    if(currentSlide == 1) {
+      slideBackward();
+      slideBackward();
+    }
+    if(currentSlide == 2) {
+      slideForward();
+      slideForward();
+    }
+    if(currentSlide == 3) {slideForward();}
+    if(currentSlide == 5) {slideBackward();}
+
+    currentSlide = 4
+  });
+
+  $('#radio5').click(function() {
+    if(currentSlide == 1) {slideBackward();}
+    if(currentSlide == 2) {
+      slideBackward();
+      slideBackward();
+    }
+    if(currentSlide == 3) {
+      slideForward();
+      slideForward();
+    }
+    if(currentSlide == 4) {slideForward();}
+
+    currentSlide = 5
+  });
 
 });
